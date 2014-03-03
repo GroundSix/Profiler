cbb0dcff6ff3a2662ebfa44bb70db78222e05aed
Harry Lawrence <harry.lawrence@groundsix.com>
2014-03-03 12:52:08 +0000

    Started Docs inline for Stats profiler

--- src/StatsProfiler.php
+++ src/StatsProfiler.php
@@ -4,5 +4,47 @@ namespace GroundSix\Component;
 
 class StatsProfiler
 {
-	
+	/**
+	 * Starts the profiling process
+	 * 
+	 * @param  String Optional message to be logged on start
+	 * 
+	 * @return Object \GroundSix\Component\StatsProfiler
+	 */
+    public function start($message = '')
+    {
+
+    }
+
+	/**
+	 * Stops the last profiling process
+	 * 
+	 * @return Null
+	 */
+    public function stop()
+    {
+
+    }
+
+	/**
+	 * Appends a message to the profile at the current time
+	 * 
+	 * @param  String Optional message to be logged on push
+	 * 
+	 * @return Null
+	 */
+    public function push($message = '')
+    {
+
+    }
+
+    public function fetch()
+    {
+
+    }
+
+    public function kill()
+    {
+
+    }
 }
