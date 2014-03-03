9a1fd61bae45edcb8e884e60f0153ee05536d389
Harry Lawrence <harry.lawrence@groundsix.com>
2014-03-03 15:17:58 +0000

    Added header comments

--- src/Model/Profile.php
+++ src/Model/Profile.php
@@ -1,9 +1,24 @@
 <?php
 
+/**
+ * Ground Six Profiler
+ *
+ * @author Andrew Willis  twitter.com/ilovefluffy
+ * @author Harry Lawrence twitter.com/harry4_
+ *
+ * (c) Ground Six 2014
+ *
+ * License: MIT
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace GroundSix\Component\Model;
 
 /**
  * Class Profile
+ * 
  * @package GroundSix\Component\Model
  */
 class Profile
--- src/NullProfiler.php
+++ src/NullProfiler.php
@@ -1,5 +1,19 @@
 <?php
 
+/**
+ * Ground Six Profiler
+ *
+ * @author Andrew Willis  twitter.com/ilovefluffy
+ * @author Harry Lawrence twitter.com/harry4_
+ *
+ * (c) Ground Six 2014
+ *
+ * License: MIT
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace GroundSix\Component;
 
 class NullProfiler implements Profiler
