fe0b98e68286981a08b5fbf514255bcb66d4b6ac
Harry Lawrence <harry.lawrence@groundsix.com>
2014-03-03 12:44:54 +0000

    Implemented NullProfiler

--- src/NullProfiler.php
+++ src/NullProfiler.php
@@ -2,7 +2,11 @@
 
 class GroundSix\Component;
 
-class NullProfiler
+class NullProfiler implements Profiler
 {
-	
+    public function start($message = '') {}
+    public function stop() {}
+    public function push($message = '') {}
+    public function fetch() {}
+    public function kill() {}
 }
--- src/Profiler.php
+++ src/Profiler.php
@@ -4,9 +4,9 @@ namespace GroundSix\Component;
 
 interface Profiler
 {
-    public function start($message);
+    public function start($message = '');
     public function stop();
-    public function push($message);
+    public function push($message = '');
     public function fetch();
     public function kill();
 }
