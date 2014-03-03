c201322e14923d7d6d534da45b6837d1a4ad9de3
Andrew Willis <andrew.willis@groundsix.com>
2014-03-03 12:46:30 +0000

    set profiler in test

--- tests/ProfileTest.php
+++ tests/ProfileTest.php
@@ -2,7 +2,19 @@
 
 namespace GroundSix\Component;
 
+use Mockery as M;
+
 class ProfileTest extends \PHPUnit_Framework_TestCase
 {
 
+    protected $profiler;
+
+    public function __construct()
+    {
+        $this->profiler = M::mock('Profiler');
+    }
+
+    public function testCreateProfile()
+    {
+    }
 } 
